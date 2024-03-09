bdf2ba743d76ca768def9cd3752308ec6b16f398
JMhica <luvlyespiritu@gmail.com>
2024-03-09 11:52:10 +0800

    added all files

---
+++ Grade System - MAYOR/Grade System - MAYOR/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MATH = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'MATH
+        '
+        Me.MATH.Location = New System.Drawing.Point(40, 59)
+        Me.MATH.Name = "MATH"
+        Me.MATH.Size = New System.Drawing.Size(330, 20)
+        Me.MATH.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(421, 667)
+        Me.Controls.Add(Me.MATH)
+        Me.Name = "Form1"
+        Me.Text = "Grade Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MATH As TextBox
+End Class
